f8a3bb41a6e2f69b444aa7179eecffa9b52844ad
dojibenty <hugo.richard2001@outlook.fr>
2023-02-10 09:54:49 +0100

    suppr classe abstraite dao factory et fonctions find/create/update/delete de Artiste,Concert,Gestionnaire

--- backend/projetSIBackend/src/main/java/DAO/DAOFactory.java
+++ backend/projetSIBackend/src/main/java/DAO/DAOFactory.java
@@ -2,7 +2,7 @@ package DAO;
 
 import data.*;
 
-public class DAOFactory extends ConcertDAOFactory {
+public class DAOFactory {
 
 	private DAOArtiste daoArtiste = null;
 	private DAOConcert daoConcert = null;
@@ -12,37 +12,31 @@ public class DAOFactory extends ConcertDAOFactory {
 	private DAOSoiree daoSoiree = null;
 
 
-	@Override
 	public DAO<Artiste> getDAOArtiste() throws DAOException {
 		if (daoArtiste == null) daoArtiste = new DAOArtiste();
 		return daoArtiste;
 	}
 
-	@Override
 	public DAO<Concert> getDAOConcert() throws DAOException {
 		if (daoConcert == null) daoConcert = new DAOConcert();
 		return daoConcert;
 	}
 
-	@Override
 	public DAO<Gestionnaire> getDAOGestionnaire() throws DAOException {
 		if (daoGestionnaire == null) daoGestionnaire = new DAOGestionnaire();
 		return daoGestionnaire;
 	}
 
-	@Override
 	public DAO<Groupe> getDAOGroupe() throws DAOException {
 		if (daoGroupe == null) daoGroupe = new DAOGroupe();
 		return daoGroupe;
 	}
 
-	@Override
 	public DAO<Salle> getDAOSalle() throws DAOException {
 		if (daoSalle == null) daoSalle = new DAOSalle();
 		return daoSalle;
 	}
 
-	@Override
 	public DAO<Soiree> getDAOSoiree() throws DAOException {
 		if (daoSoiree == null) daoSoiree = new DAOSoiree();
 		return daoSoiree;
--- backend/projetSIBackend/src/main/java/DAO/DAOGestionnaire.java
+++ backend/projetSIBackend/src/main/java/DAO/DAOGestionnaire.java
@@ -1,4 +1,60 @@
 package DAO;
 
-public class DAOGestionnaire {
+import data.Gestionnaire;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class DAOGestionnaire extends DAO<Gestionnaire> {
+
+	private final EntityManager entityManager;
+	
+	public DAOGestionnaire() throws DAOException {
+		super();
+		entityManager = Persistence.createEntityManagerFactory("SportsPU").createEntityManager();
+	}
+	
+	@Override
+	public Gestionnaire find(int id) throws DAOException {
+		Query query = entityManager.createNamedQuery("Gestionnaire.findByIdGestionnaire");
+		query.setParameter("idGestionnaire",(short)id);
+		return (Gestionnaire) query.getSingleResult();
+	}
+
+	@Override
+	public void create(Gestionnaire data) throws DAOException {
+		EntityTransaction trans = entityManager.getTransaction();
+		try {
+			trans.begin();
+			entityManager.persist(data);
+			trans.commit();
+		} catch (Exception e) {
+			if (trans != null) trans.rollback();
+		}
+	}
+
+	@Override
+	public void update(Gestionnaire data) throws DAOException {
+		EntityTransaction trans = entityManager.getTransaction();
+		try {
+			trans.begin();
+			entityManager.persist(data);
+			trans.commit();
+		} catch (Exception e) {
+			if (trans != null) trans.rollback();
+		}
+	}
+
+	@Override
+	public void delete(Gestionnaire data) throws DAOException {
+		EntityTransaction trans = entityManager.getTransaction();
+		try {
+			trans.begin();
+			entityManager.remove(data);
+			trans.commit();
+		} catch (Exception e) {
+			if (trans != null) trans.rollback();
+		}
+	}
 }
