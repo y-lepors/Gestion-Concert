194c5e35be6a28fce9974e0e08f37a15b9b22205
yanis <yanis.lepors@gmail.com>
2023-03-03 11:58:08 +0100

    Artiste aside

--- frontend/projetfrontend/src/components/ListeArtiste.vue
+++ frontend/projetfrontend/src/components/ListeArtiste.vue
@@ -1,3 +1,46 @@
+<script setup>
+
+import Artiste from "/src/components/Artiste.vue";
+import AsideArtiste from "/src/components/AsideArtiste.vue";
+
+</script>
+
+<script>
+
+	export default {
+		data() {
+			return {
+				artistes: [],
+				currentArtisteID: -1,
+			};
+		},
+		methods: {
+			setAsideArtiste(id) {
+				this.currentArtisteID = id;
+			},
+		},
+		created() {
+			fetch('http://localhost:8082/Servlet_war_exploded/getAllArtiste')
+				.then(response => response.json())
+				.then(data => {
+					this.artistes = data;
+				});
+		},
+	}
+
+</script>
+
+
 <template>
-	<h1>page2</h1>
+	<div class="parMain">
+
+	<div class="listeSalle">
+		<!-- <Salle v-for="salle in salles" :idSalle="salle.idSalle" @click="setAsideSalle(salle.id)"/> -->
+		<Artiste v-for="artiste in artistes" :nomArtiste="artiste.nom" @click="setAsideArtiste(artiste.id)"/>
+	</div>
+
+	<!-- l'attribut key force la MAJ au changement de valeur -->
+	<!-- <AsideSalle :idSalle="currentSalleID" :key="currentSalleID"/> -->
+	<AsideArtiste :idArtiste="currentArtisteID" :key="currentArtisteID"/>
+	</div>
 </template>
